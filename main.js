1708d84c1330cf3ab8b8e61592d270666aa167fe
Pablo Jurado <pjurado85@gmail.com>
2017-09-17 11:58:48 -0500

    finished challenge 2

--- main.js
+++ main.js
@@ -12,8 +12,6 @@ function mul (a, b) {
 
 // write a function identityf that takes an argument
 // and returns a function that returns that argument
-// var three = identityf(3);
-// three() --> 3
 
 function identityf (input) {
   return function () {
@@ -33,13 +31,11 @@ function addf (a) {
   }
 }
 
-console.log(addf(3)(4));
+console.assert(addf(3)(4) === 7);
 
 // write a function liftf that takes a binaty function
 // and makes it callable with two invocations
 // var addf = liftf(add);
-// addf(3)(4) --> 7
-// liftf(mul)(5)(6) --> 30
 
 function liftf (func) {
   return function (a) {
@@ -50,5 +46,36 @@ function liftf (func) {
 }
 
 var addfunc = liftf(add);
-console.log(addfunc(3)(4));
-console.log(liftf(mul)(5)(6));
+console.assert(addfunc(3)(4) === 7);
+console.assert(liftf(mul)(5)(6) === 30);
+
+
+// write a function curry that takes a binary function and an argument
+// and returns a function that can take a second argument
+
+function curry (func, a) {
+  return function (b) {
+    return func(a, b);
+  }
+}
+
+// function curryES6 (func, ...a) {
+//   return function (...b) {
+//     return func(...a, ...b);
+//   }
+// }
+
+var add3 = curry(add, 3);
+console.assert(add3(4) === 7);
+console.assert(curry(mul, 5)(6) === 30);
+
+// without writing any new functions, show 3 ways to create the inc functions
+
+var inc = curry(add, 1);
+var inc2 = liftf(add)(1);
+var inc3 = addf(1);
+
+console.assert(inc(5) === 6);
+console.assert(inc(inc(5)) === 7);
+console.assert(inc2(5) === 6);
+console.assert(inc3(5) === 6);
